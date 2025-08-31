8f4163dc8f1f7f921e085a49b817deb9e261d6b7
Vinayak Gore <vvgore2675@gmail.com>
2025-08-31 13:22:55 +0530

    Weathercard - location & responsiveness updated

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -37,7 +37,7 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ weatherData }) => {
 
   return (
     <Card className="rounded-2xl bg-gradient-to-br from-blue-500 via-indigo-500 to-purple-600 text-white shadow-xl w-full p-4 sm:p-6">
-      <div className="flex flex-col md:flex-row items-start justify-between w-full">
+      <div className="flex flex-col lg:flex-row items-start justify-between w-full">
         <motion.div
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
@@ -45,15 +45,17 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ weatherData }) => {
           className="mb-6 md:mb-0 w-full"
         >
           <CardHeader className="px-0">
-            <CardTitle className="text-5xl md:text-9xl font-extrabold tracking-wide">
-              {weatherData.location}
+            <CardTitle className="text-5xl md:text-7xl xl:text-8xl font-extrabold tracking-wide">
+              {weatherData.location.includes(",")
+                ? weatherData.location.split(",")[0]
+                : weatherData.location}
             </CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col items-start px-0">
             <span className="text-5xl md:text-7xl font-extrabold drop-shadow-lg">
               {weatherData.temperature}°C
             </span>
-            <p className="text-lg sm:text-xl mt-2 font-medium tracking-wide">
+            <p className="text-lg sm:text-xl mt-2 mb-4 font-medium tracking-wide">
               {weatherData.condition}
             </p>
           </CardContent>
@@ -65,7 +67,7 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ weatherData }) => {
           transition={{ delay: 0.2 }}
           className="flex flex-col sm:flex-row items-center justify-center gap-6 p-6 rounded-xl bg-white/20 backdrop-blur-md w-full"
         >
-          {getWeatherIcon(weatherData.condition, "size-60 md:size-80")}
+          {getWeatherIcon(weatherData.condition, "size-60 sm:size-72 lg:size-80")}
           <div className="grid grid-cols-2 gap-6">
             <div>
               <p className="text-sm opacity-80">Humidity</p>
@@ -94,21 +96,21 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ weatherData }) => {
         className="pt-6 border-t border-white/30"
       >
         <h3 className="text-2xl font-bold mb-4">5-Day Forecast</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
           {weatherData.forecast.map((day, index) => (
             <Card
               key={index}
               className="!border-0 bg-white/20 backdrop-blur-lg rounded-xl text-center hover:scale-105 transition-all duration-500 text-white"
             >
               <CardContent className="flex flex-col items-center m-auto w-full">
-                <p className="font-bold text-lg sm:text-2xl md:text-4xl">
+                <p className="font-bold text-2xl md:text-4xl">
                   {day.date
                     ? new Date(day.date).toLocaleDateString("en-US", {
                         weekday: "short",
                       })
                     : "N/A"}
                 </p>
-                {getWeatherIcon(day.condition, "size-40")}
+                {getWeatherIcon(day.condition, "size-24 lg:size-40")}
                 <p className="text-xl sm:text-2xl font-bold">
                   {day.temperature}°C
                 </p>
